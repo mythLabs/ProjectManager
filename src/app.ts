b170e4992750259115f36f69e1b3ca8f5bc02e44
mythLabs <amitbisht744@gmail.com>
2021-01-17 13:15:13 +0530

    Extracted validation to function

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,52 @@
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable): boolean {
+  let isValid = true;
+
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+
+  return isValid;
+}
+
 function autobind(
   _1: any, //target
   _2: string, //methodName
@@ -57,16 +106,40 @@ class ProjectInput {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredPeople.trim().length === 0
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
     ) {
-        alert("Invalid input, please try again")
-        return
+      alert("Invalid input, please try again");
+      return;
     } else {
-        return [enteredTitle, enteredDescription, +enteredPeople]
+      return [enteredTitle, enteredDescription, +enteredPeople];
     }
+  }
+
+  private clearForm() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
   }
 
   @autobind
@@ -74,10 +147,11 @@ class ProjectInput {
     event.preventDefault();
     const userInput = this.getUserInput();
 
-    if(Array.isArray(userInput)) {
+    if (Array.isArray(userInput)) {
       const [title, desc, people] = userInput;
-      console.log(title, desc, people)
+      console.log(title, desc, people);
     }
+    this.clearForm();
   }
 
   private configure() {
