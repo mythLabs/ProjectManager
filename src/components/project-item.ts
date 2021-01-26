68a4c01569b7cf5400f0c75221debe2ea4f71410
mythLabs <amitbisht744@gmail.com>
2021-01-26 11:53:32 +0530

    Setup webpack

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Draggable } from "../models/drag-drop.js";
-import { Component } from "../components/base.js";
-import { Project } from "../models/project.js";
-import { autobind } from "../decorators/autobind.js";
+import { Draggable } from "../models/drag-drop";
+import { Component } from "../components/base";
+import { Project } from "../models/project";
+import { autobind } from "../decorators/autobind";
 
 export class ProjectItem
   extends Component<HTMLUListElement, HTMLLIElement>
