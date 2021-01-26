f24cdd7df4ea84d72edefe90573c942692646116
mythLabs <amitbisht744@gmail.com>
2021-01-26 08:51:36 +0530

    Adding folder structure with code divided using namespace

---
+++ src/components/project-list.ts
@@ -0,0 +1,84 @@
+/// <reference path="base.ts" />
+
+namespace App {
+    export class ProjectList
+    extends Component<HTMLDivElement, HTMLElement>
+    implements DragTarget {
+    /**
+     *OOP List
+     */
+    assignedProject: any[];
+
+    constructor(private type: "active" | "finished") {
+      super("project-list", "app", false, `${type}-projects`);
+
+      this.assignedProject = [];
+
+      this.configure();
+      this.renderContent();
+    }
+
+    @autobind
+    dragOverHandler(event: DragEvent): void {
+      if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+        const listEl = this.element.querySelector("ul")!;
+        listEl.classList.add("droppable");
+      }
+    }
+
+    @autobind
+    dropHandler(event: DragEvent): void {
+      const prjId = event.dataTransfer!.getData("text/plain");
+      projectState.moveProject(
+        prjId,
+        this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+      );
+
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.remove("droppable");
+    }
+
+    @autobind
+    dragLeaveHandler(_: DragEvent): void {
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.remove("droppable");
+    }
+
+    configure() {
+      this.element.addEventListener("dragover", this.dragOverHandler);
+      this.element.addEventListener("dragleave", this.dragLeaveHandler);
+      this.element.addEventListener("drop", this.dropHandler);
+
+      projectState.addListeners((projects: Project[]) => {
+        const relevantProjects = projects.filter((prj) => {
+          if (this.type == "active") {
+            return prj.status === ProjectStatus.Active;
+          }
+          return prj.status === ProjectStatus.Finished;
+        });
+        this.assignedProject = relevantProjects;
+        this.renderProjects();
+      });
+    }
+
+    renderContent() {
+      const listId = `${this.type}-projects-list`;
+      this.element.querySelector("ul")!.id = listId;
+      this.element.querySelector(
+        "h2"
+      )!.textContent = `${this.type.toUpperCase()} PROJECT`;
+    }
+
+    private renderProjects() {
+      const listEl = document.getElementById(
+        `${this.type}-projects-list`
+      )! as HTMLElement;
+
+      listEl.innerHTML = "";
+      for (const prjItem of this.assignedProject) {
+        new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
+      }
+    }
+  }
+}
